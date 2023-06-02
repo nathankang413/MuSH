acf6f5dbd4bbd7b16f85911d024053c1bc515c5f
Nathan Kang <nakang@unix5.csc.calpoly.edu>
2023-06-02 11:11:51 -0700

    -vp option is correct

--- mush2.c
+++ mush2.c
@@ -2,7 +2,9 @@
 
 int main(int argc, char *argv[]) {
     int o, v=0, p=0;
-    char *cmd_file=NULL;
+    char *cmd_path=NULL, *line;
+    FILE *cmd_file=stdin;
+    struct pipeline *pl;
 
     /* parse options */
     while ((o = getopt(argc, argv, "vp")) != -1) {
@@ -22,9 +24,15 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    /* get command file, if exists */
+    /* setup command file, if exists */
     if (argc - optind == 1) {
-        cmd_file = argv[optind++];
+        cmd_path = argv[optind++];
+    }
+    if (cmd_path) {
+        if (!(cmd_file = fopen(cmd_path, "r"))) {
+            perror(cmd_path);
+            return EXIT_FAILURE;
+        }
     }
 
     /* verbose: print options */
@@ -32,7 +40,32 @@ int main(int argc, char *argv[]) {
         printf("Options:\n");
         printf("  int   opt_verbose   = %d\n", v);
         printf("  int   opt_parseonly = %d\n", p);
-        printf("  char *opt_mode      = %s\n", cmd_file ? cmd_file : "(null)");
+        printf("  char *opt_mode      = %s\n", cmd_path ? cmd_path : "(null)");
+        fflush(stdout);
+    }
+
+    if (!p) { /* todo: implement parseonly */
+        fprintf(stderr, "mush2: only parseonly mode has been implemented\n");
+        return EXIT_FAILURE;
+    }
+
+    /* CLI loop */
+    while (!feof(cmd_file)) {
+        if (!(line = readLongString(cmd_file))) {
+            fprintf(stderr, "readLongString: clerror %d\n", clerror);
+            continue;
+        }
+        if (!(pl = crack_pipeline(line))) {
+            /* fprintf(stderr, "crack_pipeline: clerror %d\n", clerror); */
+            continue;
+        }
+
+        if (v) {
+            print_pipeline(stdout, pl);
+        }
+
+        free(line);
+        free_pipeline(pl);
     }
 
     return EXIT_SUCCESS;
--- mush2.h
+++ mush2.h
@@ -2,5 +2,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <mush.h>
 
 #define USAGE "usage: %s [ -v ] [ infile ]\n", argv[0]

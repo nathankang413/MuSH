539fe5284e4db893910449483bc2b15fcdef8f3c
Nathan Kang <nakang@unix5.csc.calpoly.edu>
2023-06-02 13:00:52 -0700

    IO redirection, but no pipes

--- mush2.c
+++ mush2.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
     char *cmd_path=NULL, *line;
     FILE *cmd_file=stdin;
     struct pipeline *pl;
+    pid_t child;
+    int in_fd, out_fd, status;
 
     /* parse options */
     while ((o = getopt(argc, argv, "vp")) != -1) {
@@ -44,17 +46,22 @@ int main(int argc, char *argv[]) {
         fflush(stdout);
     }
 
-    if (!p) { /* todo: implement parseonly */
-        fprintf(stderr, "mush2: only parseonly mode has been implemented\n");
-        return EXIT_FAILURE;
-    }
+    /* todo: handle sigint */
 
     /* CLI loop */
     while (!feof(cmd_file)) {
+
+        if (isatty(fileno(cmd_file))) {
+            printf("8-P ");
+            fflush(stdout);
+        }
+
         if (!(line = readLongString(cmd_file))) {
-            fprintf(stderr, "readLongString: clerror %d\n", clerror);
+            if (clerror)
+                fprintf(stderr, "readLongString: clerror %d\n", clerror);
             continue;
         }
+
         if (!(pl = crack_pipeline(line))) {
             /* fprintf(stderr, "crack_pipeline: clerror %d\n", clerror); */
             continue;
@@ -64,9 +71,71 @@ int main(int argc, char *argv[]) {
             print_pipeline(stdout, pl);
         }
 
+        if (!p && pl->length > 0) { /* actually run the command */
+            /* for just one command (todo) */
+            if (pl->length > 1) {
+                fprintf(stderr, "mush2: only one command has been implemented\n");
+                continue;
+            }
+
+            /* open infile for reading */
+            in_fd = STDIN_FILENO;
+            if (pl->stage->inname) {
+                if ((in_fd = open(pl->stage->inname, O_RDONLY)) == -1) {
+                    perror(pl->stage->inname);
+                    continue;
+                }
+            }
+
+            /* open outfile for writing */
+            out_fd = STDOUT_FILENO;
+            if (pl->stage->outname) {
+                if ((out_fd = open(pl->stage->outname, 
+                    O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+                    perror(pl->stage->outname);
+                    continue;
+                }
+            }
+
+            /* fork off child process */
+            if ((child = fork()) == -1) {
+                perror("fork");
+                continue;
+            }
+
+            /* child will call exec */
+            if (child == 0) {
+                if (dup2(in_fd, STDIN_FILENO) != in_fd) {
+                    close(in_fd);
+                }
+                if (dup2(out_fd, STDOUT_FILENO) != out_fd) {
+                    close(out_fd);
+                }
+                execvp(pl->stage->argv[0], pl->stage->argv);
+                perror(pl->stage->argv[0]);
+                return EXIT_FAILURE;
+            }
+
+            /* close parent's fds to pipes or IO redirection */
+            if (in_fd != STDIN_FILENO)
+                close(in_fd);
+            if (out_fd != STDOUT_FILENO)
+                close(out_fd);
+
+            /* parent will wait for child to die */
+            if (waitpid(child, &status, 0) == -1) {
+                perror("waitpid");
+                return EXIT_FAILURE;    /* todo: should this be continue? */
+            }
+
+            /* todo: look at status? */
+
+        }
+
         free(line);
         free_pipeline(pl);
     }
 
+    yylex_destroy();
     return EXIT_SUCCESS;
 }
--- mush2.h
+++ mush2.h
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <mush.h>
 
 #define USAGE "usage: %s [ -v ] [ infile ]\n", argv[0]
